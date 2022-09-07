6a73ef5c1092f0162a5b8ef55c38da57f0431c68
realib1 <ibeevpersie@gmail.com>
2022-09-07 15:07:28 +0000

    modified:   index.html 	modified:   script.js 	modified:   services.html 	modified:   style.css

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const barMenu = document.querySelector(".hamburger"),
   navMenu = document.querySelector(".nav-menu"),
   navLink = document.querySelectorAll(".nav-link"),
   main = document.querySelector(".main"),
-  toTop = document.getElementById("back-to-top");
+  toTop = document.getElementById("back-to-top"),
+  bars = document.querySelector(".open-close")
 
 const openNav = () => {
   barMenu.classList.toggle("active");
   navMenu.classList.toggle("active");
+  bars.innerHTML = `<i class="fas fa-times"></i>`;
+
 };
 
 const closeNav = () => {
@@ -92,3 +95,32 @@ function headAnimate() {
     }
   });
 }
+
+// Theme Toggle
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem("theme");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("color-scheme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+const switchTheme = (e) => {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("color-scheme", "dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.setAttribute("color-scheme", "light");
+      localStorage.setItem("theme", "light");
+    }
+  }
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
